724b92103a0be217cc94ad2878f23fd34a55f485
npablo1987 <kyovilches@gmail.com>
2023-01-12 11:40:56 -0400

    Initial commit

---
+++ src/main/java/com/pvilches/utilizaciondatosbbdd/app/UtilizacionDatosBbddApplication.java
@@ -0,0 +1,13 @@
+package com.pvilches.utilizaciondatosbbdd.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilizacionDatosBbddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilizacionDatosBbddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pvilches/utilizaciondatosbbdd/app/UtilizacionDatosBbddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvilches.utilizaciondatosbbdd.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilizacionDatosBbddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
